ba6e5f585a9cf5212cdc08b34d91f0f01928d5ff
HenryXanderTalent <henry.opara@xandertalent.com>
2023-04-12 12:35:10 +0100

    setup app output

--- OpenWeatherAPI-Get/requests.py
+++ OpenWeatherAPI-Get/requests.py
@@ -1,5 +1,11 @@
 import requests
 
+cityName = input('Choose a location (by city): ')
+
+APIkey = 'd43fa3449113ba8f1b2b43193a35444e'
+
+data = None
+
 #GET by location request
 response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={APIkey}')
 if response.status_code == 200:
@@ -13,3 +19,5 @@ else:
 temperature = data['main']['temp']
 humidity = data['main']['humidity']
 desc = data['weather'][0]['description']
+
+print(f'Temperature in {cityName} is {temperature} with a humidity of {humidity} and can be described as \"{desc}\"')

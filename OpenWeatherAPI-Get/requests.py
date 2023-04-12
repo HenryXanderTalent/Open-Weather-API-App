b2c089f20750636866a2983bdad5df8a3d9fc2c6
HenryXanderTalent <henry.opara@xandertalent.com>
2023-04-12 12:32:20 +0100

    setup request method

--- OpenWeatherAPI-Get/requests.py
+++ OpenWeatherAPI-Get/requests.py
@@ -1,1 +1,15 @@
 import requests
+
+#GET by location request
+response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={APIkey}')
+if response.status_code == 200:
+    data = response.json()
+    #print(data)
+else:
+    print('Error:', response.status_code)
+
+#add try catch
+
+temperature = data['main']['temp']
+humidity = data['main']['humidity']
+desc = data['weather'][0]['description']
